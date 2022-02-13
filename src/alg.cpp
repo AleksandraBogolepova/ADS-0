9abe94424217e12d476042478913d0d811d3f015
AleksandraBogolepova <99176112+AleksandraBogolepova@users.noreply.github.com>
2022-02-13 04:23:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,14 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
+    if (a == b) {
+        return a;
+    }
+    if (a > b) {
+        int s = a;
+        a = b;
+        b = s;
+    }
+    return gcd(a, b - a);
     // поместить сюда текст реализации алгоритма (на С++)
 }
